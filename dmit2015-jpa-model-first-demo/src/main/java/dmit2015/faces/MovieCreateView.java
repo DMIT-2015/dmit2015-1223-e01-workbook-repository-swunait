243858231884163248c00528567e102b5bd2e9ba
Sam Wu <swu@nait.ca>
2023-06-16 20:00:01 -0600

    added CRUD example on Movie entity

---
+++ dmit2015-jpa-model-first-demo/src/main/java/dmit2015/faces/MovieCreateView.java
@@ -0,0 +1,51 @@
+package dmit2015.faces;
+
+import dmit2015.entity.Movie;
+import dmit2015.persistence.MovieRepository;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import org.omnifaces.util.Messages;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named("currentMovieCreateView")
+@RequestScoped
+public class MovieCreateView {
+
+    @Inject
+    private MovieRepository _movieRepository;
+
+    @Getter
+    private Movie newMovie = new Movie();
+
+    @Getter @Setter
+    private String selectedGenre;
+    @Getter
+    private String[] availableGenres = {"Action", "Comedy", "Crime", "Horror","Thrillers","Fantasy","Science-Fiction"};
+
+    @PostConstruct  // After @Inject is complete
+    public void init() {
+
+    }
+
+    public String onCreateNew() {
+        String nextPage = "";
+        try {
+//            newMovie.setGenre(selectedGenre);
+            _movieRepository.add(newMovie);
+            Messages.addFlashGlobalInfo("Create was successful. {0}", newMovie.getId());
+            nextPage = "index?faces-redirect=true";
+        } catch (RuntimeException ex) {
+            Messages.addGlobalWarn(ex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Messages.addGlobalError("Create was not successful. {0}", e.getMessage());
+        }
+        return nextPage;
+    }
+
+}
