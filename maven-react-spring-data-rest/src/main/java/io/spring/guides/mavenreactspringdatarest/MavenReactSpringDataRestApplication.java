f68ecbe64a2062f1b6041cdac4a99162f5c834a4
Sam Wu <swu@nait.ca>
2023-08-16 20:39:47 -0600

    added Spring REST and React tutorial

---
+++ maven-react-spring-data-rest/src/main/java/io/spring/guides/mavenreactspringdatarest/MavenReactSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package io.spring.guides.mavenreactspringdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReactSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenReactSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ maven-react-spring-data-rest/src/test/java/io/spring/guides/mavenreactspringdatarest/MavenReactSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.guides.mavenreactspringdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenReactSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
